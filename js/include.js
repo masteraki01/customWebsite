1b17eab027a74724ad66c85456a9f22a4c3958f4
Johnson Do <16851841+masteraki01@users.noreply.github.com>
2026-02-13 04:38:11 -0800

    theme button injected into navbars

--- js/include.js
+++ js/include.js
@@ -1,31 +1,3 @@
-fetch("components/navbar.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("navbar").innerHTML = data;
-  })
-  .catch(err => console.error("Navbar load failed:", err));
-
-fetch("components/menumain.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("menumain").innerHTML = data;
-  })
-  .catch(err => console.error("menumain load failed:", err));
-
-
-fetch("components/menu.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("menu-list").innerHTML = data;
-  })
-  .catch(err => console.error("menu-list load failed:", err));
-
-  fetch("components/footer.html")
-  .then(response => response.text())
-  .then(data => {
-    document.getElementById("footer").innerHTML = data;
-  })
-  .catch(err => console.error("footer load failed:", err));
 
 // ===== THEME SYSTEM =====
 const themes = [
@@ -66,3 +38,32 @@ window.addEventListener("storage", function (e) {
     document.documentElement.classList.add(e.newValue);
   }
 });
+
+fetch("components/navbar.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("navbar").innerHTML = data;
+  })
+  .catch(err => console.error("Navbar load failed:", err));
+
+fetch("components/menumain.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("menumain").innerHTML = data;
+  })
+  .catch(err => console.error("menumain load failed:", err));
+
+
+fetch("components/menu.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("menu-list").innerHTML = data;
+  })
+  .catch(err => console.error("menu-list load failed:", err));
+
+  fetch("components/footer.html")
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("footer").innerHTML = data;
+  })
+  .catch(err => console.error("footer load failed:", err));
